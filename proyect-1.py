793426299b439670638097ee5cd44b19dcee5e43
jibo16 <jorgevd2010@hotmail.com>
2020-01-12 19:22:38 -0600

    display pantall

---
+++ proyect-1.py
@@ -0,0 +1,20 @@
+
+
+import pygame, sys, random
+
+from pygame.locals import *
+
+
+pygame.init()
+
+DISPLAYSURF = pygame.display.set_mode((400, 300))
+
+pygame.display.set_caption('Hello World!')
+x = 10
+y = 0
+while n <= x:
+	print('hello world')
+	y+=1
+	break
+	
+
